34a616f3634539fd43ba54e7b31c8a3f1f99cd4b
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-08-30 14:23:32 -0600

    Archivo de prueba

--- archivoPrueba.sh
+++ archivoPrueba.sh
@@ -1,11 +1,11 @@
 #Archivo de prueba
 
 #CREACION DE DISCOS
-Mkdisk -size=50 -unit=M -path=/home/archivos/fase1/Disco1.disk
-Mkdisk -unit=k -size=51200 -path=/home/archivos/fase1/Disco2.disk
+Mkdisk -size=50 -unit=M -path=/home/archivos/fase1/Disco1.disk -fit=FF
+Mkdisk -unit=k -size=51200 -path=/home/archivos/fase1/Disco2.disk -fit=BF
 mkDisk -size=10 -path=/home/archivos/fase1/Disco3.disk
 mkdisk -size=51200 -path="/home/archivos/fase1/mis archivos/Disco4.disk" -unit=K
-mkDisk -size=20 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -unit=M
+mkDisk -size=20 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -unit=M -fit=WF
 
 #Deberia dar error
 mkdisk -param=x -size=30 -path=/home/archivos/fase1/Disco.disk
@@ -23,13 +23,13 @@ fdisk -type=E -unit=K -name=Part2 -size=7680 -path=/home/archivos/fase1/Disco1.d
 fdisk -type=E -unit=K -name=Part3 -size=7680 -path=/home/archivos/fase1/Disco1.disk -fit=WF #Deberia dar error
 fdisk -type=P -unit=K -name=Part3 -size=7680 -path=/home/archivos/fase1/Disco1.disk -fit=WF
 fdisk -type=P -unit=K -name=Part4 -size=7680 -path=/home/archivos/fase1/Disco1.disk -fit=BF
-FDISK -type=L -unit=k -name=part5 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=BF #1.25 MB
-fdisk -type=L -unit=K -name=part6 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=WF
-fdisk -type=L -unit=K -name=part7 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=wf
-fdisk -type=L -unit=K -name=part8 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=ff
-fdisk -type=L -unit=K -name=part9 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=bf
-fdisk -type=L -unit=K -name=part10 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=wf
-fdisk -type=L -unit=K -name=part10 -size=1024 -path=/home/archivos/fase1/Disco1.disk -fit=BF #ERROR nombre y espacio
+FDISK -type=L -unit=k -name=Part5 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=BF #1.25 MB
+fdisk -type=L -unit=K -name=Part6 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=WF
+fdisk -type=L -unit=K -name=Part7 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=wf
+fdisk -type=L -unit=K -name=Part8 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=ff
+fdisk -type=L -unit=K -name=Part9 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=bf
+fdisk -type=L -unit=K -name=Part10 -size=1280 -path=/home/archivos/fase1/Disco1.disk -fit=wf
+fdisk -type=L -unit=K -name=Part10 -size=1024 -path=/home/archivos/fase1/Disco1.disk -fit=BF #ERROR nombre y espacio
 
 #Particiones en el disco2
 fdisk -type=L -unit=k -name=Part11 -size=10240 -path=/home/archivos/fase1/Disco2.disk -fit=BF #ERRROR no hay una extendida
@@ -56,7 +56,7 @@ fdisk -type=L -unit=k -name=Part29 -size=1536 -path=/home/archivos/fase1/Disco2.
 
 #Particiones en el disco 5
 fdisk -type=E -unit=k -name=Part30 -size=5120 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF #5MB
-fdisk -type=L -unit=k -name=Part31 -size=1024-path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
+fdisk -type=L -unit=k -name=Part31 -size=1024 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
 fdisk -type=P -unit=k -name=Part32 -size=5120 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
 fdisk -type=L -unit=k -name=Part33 -size=1024 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
 fdisk -type=P -unit=k -name=Part34 -size=5120 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
@@ -64,28 +64,37 @@ fdisk -type=P -unit=k -name=Part35 -size=5120 -path="/home/archivos/fase1/mis ar
 fdisk -type=L -unit=k -name=Part36 -size=1024 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
 fdisk -type=L -unit=k -name=Part37 -size=1024 -path="/home/archivos/fase1/mis archivos/Disco5.disk" -fit=BF
 
+#Eliminar particiones disco1
+fdisk -delete=fast -name=Part1 -path=/home/archivos/fase1/Disco1.disk
+fdisk -delete=fast -name=Part6 -path=/home/archivos/fase1/Disco1.disk
+fdisk -delete=fast -name=Part10 -path=/home/archivos/fase1/Disco1.disk
+fdisk -delete=fast -name=Part9 -path=/home/archivos/fase1/Disco1.disk
+#Agregando espacio
+fdisk -add=500 -unit=K -path=/home/archivos/fase1/Disco1.disk -name=Part4
+
+#Eliminadndo y agregando espacio dusci2
+fdisk -delete=fast -name=Part13 -path=/home/archivos/fase1/Disco2.disk
+fdisk -add=1 -unit=m -path=/home/archivos/fase1/Disco2.disk -name=Part12
+
 #MONTAR PARTICIONES
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part1 #vda1
 mount -path=/home/archivos/fase1/Disco3.disk -name=Part3 #ERROR no existe
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part2 #vda2
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part3 #vda3
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part4 #vda4
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part5 #vda5
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part6 #vda6
-mount -path=/home/archivos/fase1/Disco1.disk -name=Part7 #vda7
-mount -path=/home/archivos/fase1/Disco2.disk -name=Part13 #vdb1
-mount -path=/home/archivos/fase1/Disco2.disk -name=Part14 #vdb2
-mount -path=/home/archivos/fase1/Disco2.disk -name=Part15 #vdb3
-mount -path=/home/archivos/fase1/Disco2.disk -name=Part16 #vdb4
-mount -path=/home/archivos/fase1/Disco2.disk -name=Part17 #vdb5
+mount -path=/home/archivos/fase1/Disco1.disk -name=Part2 #vda1
+mount -path=/home/archivos/fase1/Disco1.disk -name=Part3 #vda2
+mount -path=/home/archivos/fase1/Disco1.disk -name=Part4 #vda3
+mount -path=/home/archivos/fase1/Disco1.disk -name=Part5 #vda4
+mount -path=/home/archivos/fase1/Disco1.disk -name=Part7 #vda5
+mount -path=/home/archivos/fase1/Disco2.disk -name=Part14 #vdb1
+mount -path=/home/archivos/fase1/Disco2.disk -name=Part15 #vdb2
+mount -path=/home/archivos/fase1/Disco2.disk -name=Part16 #vdb3
+mount -path=/home/archivos/fase1/Disco2.disk -name=Part17 #vdb4
 mount -path="/home/archivos/fase1/mis archivos/Disco5.disk" -name=Part30 #vdc1
 mount -path="/home/archivos/fase1/mis archivos/Disco5.disk" -name=Part31 #vdc2
 mount -path="/home/archivos/fase1/mis archivos/Disco5.disk" -name=Part32 #vdc3
 
 #DESMONTAR PARTICIONES
+unmount -id=vda3
 unmount -id=vda5
-unmount -id=vda6
-unmount -id=vdb5
+unmount -id=vdb4
 unmount -id=vdc2
 unmount -id=vdx1 #ERROR
 
@@ -95,6 +104,6 @@ rep -id=vda1 -Path=/home/reports/reporte2.jpg -name=mbr
 rep -id=vdb1 -path=/home/reports/reporte3.jpg -name=disk
 rep -id=vdb1 -path=/home/reports/reporte4.jpg -name=mbr
 rep -id=vdc1 -path=/home/reports/reporte5.jpg -name=disk
-rep -id=vdc1 -path=/home/reports/report6.jpg -name=mbr
+rep -id=vdc1 -path=/home/reports/reporte6.jpg -name=mbr
 
-exec -path=/home/raul/Desktop/archivo_de_prueba.sh 
+#exec -path=/home/raul/Desktop/archivoPrueba.sh
